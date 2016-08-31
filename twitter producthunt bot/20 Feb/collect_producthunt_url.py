f48fe47ee560aed9d2dba6cd433770418f41d63e
ravishankar1788 <ravi.shankar1788@gmail.com>
2016-08-31 18:02:14 +0530

    11

--- twitter producthunt bot/20 Feb/collect_producthunt_url.py
+++
@@ -1,9 +0,0 @@
-for tweet in alltweets:
-                t = tweet.split()
-                for i in t:
-                        if 'http' in i:
-                                r = requests.get(i)
-                                if 'x-twitter-response-tags' not in r.headers.keys():
-                                        links.append(i)
-                                        c.execute("INSERT INTO links (link) values(?)", (i, ))
-                                        conn.commit()
